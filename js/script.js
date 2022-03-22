e154451c993d5b15a15ecd944e22fa3b5694d75a
Yevhenii Orhanistyi <88982441+yevheniiorhanistyi@users.noreply.github.com>
2022-03-22 15:08:46 +0100

    js

--- js/script.js
+++ js/script.js
@@ -1,36 +1,103 @@
-(function() {
- 'use strict';
-
-var goTop = document.querySelector('.toTop');
-
-window.addEventListener('scroll', trackScroll);
-
-
-function trackScroll() {
-    var scrolled = window.pageYOffset;
-    var coords = document.documentElement.clientHeight;
-
-    if (scrolled > 500) {
-      goTop.classList.add('active');
-    }
-    if (scrolled < 500) {
-      goTop.classList.remove('active');
-    }
-  }
-
-
-})();
-
+"use strict";
 
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.header-list');
 
-  menuBtn.addEventListener('click', function(){
+menuBtn.addEventListener('click', clicked);
+menu.addEventListener('click', clicked);
+
+function clicked() {
   menuBtn.classList.toggle('active');
   menu.classList.toggle('active');
+
+  if (menuBtn.classList.contains('active')) {
+    hideScroll();
+  }
+  else {
+    showScroll();
+  }
+}
+
+const hideScroll = () => {
+  const scrollWidth = `${getScrollbarWidth()}px`;
+
+  document.body.style.paddingRight = scrollWidth;
+  document.body.style.overflow = 'hidden';
+  document.getElementById('menu-btn').style.paddingRight = scrollWidth;
+};
+const showScroll = () => {
+  document.body.style.paddingRight = '';
+  document.body.style.overflow = 'visible';
+  document.getElementById('menu-btn').style.paddingRight = '';
+};
+
+const resetNav = () => {
+  document.querySelector('.menu-btn').classList.remove('active');
+  document.querySelector('.header-list').classList.remove('active');
+  showScroll();
+};
+
+window.addEventListener('resize', resetNav);
+
+// Get scrollbar width
+const getScrollbarWidth = () => {
+
+  const outer = document.createElement('div');
+
+  outer.style.position = 'absolute';
+  outer.style.top = '-9999px';
+  outer.style.width = '50px';
+  outer.style.height = '50px';
+  outer.style.overflow = 'scroll';
+  outer.style.visibility = 'hidden';
+
+  document.body.appendChild(outer);
+  const scrollbarWidth = outer.offsetWidth - outer.clientWidth;
+  document.body.removeChild(outer);
+
+  return scrollbarWidth;
+};
+
+// box-menu; menu-items
+
+let lists = document.querySelectorAll('.menu-lists__list-item');
+
+lists.forEach(onTabClick);
+
+function onTabClick(item) {
+  item.addEventListener('click', function (event) {
+    let currentLink = item;
+    let tabId = currentLink.getAttribute("data-tab");
+    let currentTab = document.querySelector(tabId);
+
+    currentTab.classList.toggle('swiper-block--active');
+    event.preventDefault();
+  });
+}
+// slider
+
+new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: 4,
+  breakpointsInverse: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+
+    470: {
+      slidesPerView: 2,
+    },
+
+    684: {
+      slidesPerView: 3,
+    },
+
+    912: {
+      slidesPerView: 4,
+    },
+
+  }
 });
 
-  menu.addEventListener('click', function(){
-  menuBtn.classList.toggle('active');
-  menu.classList.toggle('active');
-});
+
